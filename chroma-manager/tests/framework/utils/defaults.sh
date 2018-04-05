whamcloud/integrated-manager-for-lustre

use copr fork for Jenkins testing
Signed-off-by: Tom Nabarro <tom.nabarro@intel.com>

chroma-manager/tests/framework/utils/defaults.sh
@@ -105,7 +105,7 @@ set_defaults() {
 
     export CLUSTER_CONFIG="cluster_cfg.json"
 
-    export COPR_OWNER="managerforlustre"
+    export COPR_OWNER="tanabarr"
     export COPR_PROJECT="manager-for-lustre"
     #LUSTRE_REVIEW_BUILD="12345"
     if [ -n "$LUSTRE_REVIEW_BUILD" ]; then

chroma-manager/tests/framework/utils/install_deps.sh
@@ -4,5 +4,5 @@ mkdir -p chroma_test_env
 tar -C chroma_test_env -xizf -"
 
 ssh root@$CHROMA_MANAGER "set -ex
-yum-config-manager --add-repo https://copr.fedorainfracloud.org/coprs/managerforlustre/manager-for-lustre/repo/epel-7/managerforlustre-manager-for-lustre-epel-7.repo
+yum-config-manager --add-repo https://copr.fedorainfracloud.org/coprs/tanabarr/manager-for-lustre/repo/epel-7/managerforlustre-manager-for-lustre-epel-7.repo
 yum -y install nodejs npm nginx libuv iml-gui iml-srcmap-reverse iml-online-help"

alotofshit/intel-manager-for-lustre

chroma-manager/tests/framework/utils/defaults.sh
@@ -105,7 +105,7 @@ set_defaults() {
 
     export CLUSTER_CONFIG="cluster_cfg.json"
 
-    export COPR_OWNER="managerforlustre"
+    export COPR_OWNER="joegrund"
     export COPR_PROJECT="manager-for-lustre"
     #LUSTRE_REVIEW_BUILD="12345"
     if [ -n "$LUSTRE_REVIEW_BUILD" ]; then

chroma-manager/tests/framework/utils/install_deps.sh
@@ -4,5 +4,5 @@ mkdir -p chroma_test_env
 tar -C chroma_test_env -xizf -"
 
 ssh root@$CHROMA_MANAGER "set -ex
-yum-config-manager --add-repo https://copr.fedorainfracloud.org/coprs/managerforlustre/manager-for-lustre/repo/epel-7/managerforlustre-manager-for-lustre-epel-7.repo
+yum-config-manager --add-repo https://copr.fedorainfracloud.org/coprs/joegrund/manager-for-lustre/repo/epel-7/joegrund-manager-for-lustre-epel-7.repo
 yum -y install nodejs npm nginx libuv iml-gui iml-srcmap-reverse iml-online-help"